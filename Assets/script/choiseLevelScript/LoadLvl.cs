bf23c17f4e6fce5a321dfbeb6a4c54c0bf0a2c72
dizze <mrdizzerr@gmail.com>
2023-05-29 20:38:23 +0300

    choice of the level
    choice of the level was added

---
+++ Assets/script/choiseLevelScript/LoadLvl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLvl : MonoBehaviour
+{
+    public void Load1lvl(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void Load2lvl(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void Load3lvl(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void Load4lvl(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void Load5lvl(){
+        SceneManager.LoadScene(4);
+    }
+
+    public void Load6lvl(){
+        SceneManager.LoadScene(5);
+    }
+
+    public void Load7lvl(){
+        SceneManager.LoadScene(6);
+    }
+    
+    public void Load8lvl(){
+        SceneManager.LoadScene(7);
+    }
+
+    public void Load9lvl(){
+        SceneManager.LoadScene(8);
+    }
+
+    public void Load10lvl(){
+        SceneManager.LoadScene(9);
+    }
+}
